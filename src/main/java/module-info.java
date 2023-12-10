0adfc2d27c7e7aecea22a59d947afa438bd40cbb
Duc Minh Pham <ducminhpham20v2@gmail.com>
2023-12-11 00:04:36 +0700

    Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,5 +12,6 @@ module com.example.frontend {
     requires com.almasb.fxgl.all;
 
     opens com.example.frontend to javafx.fxml;
+    opens com.example.frontend.controller to javafx.fxml;
     exports com.example.frontend;
 }
